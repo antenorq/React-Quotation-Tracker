44b6f46b7e9f58248702acc3d039bd62836a6822
antenorq <antenorq@gmail.com>
2023-08-01 23:30:03 -0400

    customer added to mongodb + customer validation

--- backend/controllers/CustomerController.js
+++ backend/controllers/CustomerController.js
@@ -1,10 +1,23 @@
 const Customer = require("../models/Customer");
-const mongoose = require("mongoose");
 
 // ADD CUSTOMER
 const add = async (req, res) => {
   const { name, business, email, phone, address } = req.body;
 
+  //check if customer exists
+  const email_customer = await Customer.findOne({ email });
+  const phone_customer = await Customer.findOne({ phone });
+
+  if (email_customer) {
+    res.status(422).json({ errors: ["This customer email already exist"] });
+    return;
+  }
+  if (phone_customer) {
+    res.status(422).json({ errors: ["This customer phone already exist"] });
+    return;
+  }
+
+  //Add Customer
   const newCustomer = await Customer.create({
     name,
     business,
@@ -13,115 +26,92 @@ const add = async (req, res) => {
     address,
   });
 
-  res.send("add customer");
-  //   //check if user exists
-  //   const user = await User.findOne({ email });
-
-  //   if (user) {
-  //     res.status(422).json({ errors: ["This email already exist"] });
-  //     return;
-  //   }
-
-  //   //Generate password hash
-  //   const salt = await bcrypt.genSalt();
-  //   const passwordHash = await bcrypt.hash(password, salt);
-
-  //   //Create User
-  //   const newUser = await User.create({
-  //     name,
-  //     email,
-  //     password: passwordHash,
-  //   });
-
-  //   // If Something went wrong return error
-  //   if (!newUser) {
-  //     res.status(422).json({ errors: ["Something went wrong, try again later"] });
-  //   }
-
-  //   // If use was created successfully, return the token
-  //   res.status(201).json({
-  //     _id: newUser._id,
-  //     token: generateToken(newUser._id),
-  //   });
-  // };
-
-  // //SIGN IN
-  // const login = async (req, res) => {
-  //   const { email, password } = req.body;
-
-  //   const user = await User.findOne({ email });
-
-  //   //check if user exists
-  //   if (!user) {
-  //     res.status(404).json({ errors: ["User not exists"] });
-  //     return;
-  //   }
-
-  //   //check if password matches
-  //   if (!(await bcrypt.compare(password, user.password))) {
-  //     res.status(422).json({ errors: ["Invalid password"] });
-  //     return;
-  //   }
-
-  //   //return user with token
-  //   res.status(201).json({
-  //     _id: user._id,
-  //     token: generateToken(user._id),
-  //   });
-};
-
-//GET CURRENT LOGGED IN USER
-const getCurrentUser = async (req, res) => {
-  const user = req.user;
-  res.status(200).json(user);
-};
-
-//UPDATE AN USER
-const update = async (req, res) => {
-  const { name, email, password } = req.body;
-
-  const reqUser = req.user;
-  const user = await User.findById(reqUser._id).select("-password");
-
-  if (name) {
-    user.name = name;
+  // If Something went wrong return error
+  if (!newCustomer) {
+    res.status(422).json({ errors: ["Something went wrong, try again later"] });
   }
 
-  if (email) {
-    user.email = email;
-  }
-
-  if (password) {
-    //Generate password hash
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    user.password = passwordHash;
-  }
-
-  await user.save();
-
-  res.status(200).json(user);
+  // If customer was created successfully, return customer
+  res.status(201).json(newCustomer);
 };
 
-//GET USER BY ID
-const getUserById = async (req, res) => {
-  const { id } = req.params;
+// //SIGN IN
+// const login = async (req, res) => {
+//   const { email, password } = req.body;
 
-  try {
-    const user = await User.findById(id).select("-password");
+//   const user = await User.findOne({ email });
 
-    //check if user exists
-    if (!user) {
-      res.status(404).json({ errors: ["User not exist"] });
-      return;
-    }
+//   //check if user exists
+//   if (!user) {
+//     res.status(404).json({ errors: ["User not exists"] });
+//     return;
+//   }
 
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(404).json({ errors: ["User not exist"] });
-  }
-};
+//   //check if password matches
+//   if (!(await bcrypt.compare(password, user.password))) {
+//     res.status(422).json({ errors: ["Invalid password"] });
+//     return;
+//   }
+
+//   //return user with token
+//   res.status(201).json({
+//     _id: user._id,
+//     token: generateToken(user._id),
+//   });
+// };
+
+// //GET CURRENT LOGGED IN USER
+// const getCurrentUser = async (req, res) => {
+//   const user = req.user;
+//   res.status(200).json(user);
+// };
+
+// //UPDATE AN USER
+// const update = async (req, res) => {
+//   const { name, email, password } = req.body;
+
+//   const reqUser = req.user;
+//   const user = await User.findById(reqUser._id).select("-password");
+
+//   if (name) {
+//     user.name = name;
+//   }
+
+//   if (email) {
+//     user.email = email;
+//   }
+
+//   if (password) {
+//     //Generate password hash
+//     const salt = await bcrypt.genSalt();
+//     const passwordHash = await bcrypt.hash(password, salt);
+
+//     user.password = passwordHash;
+//   }
+
+//   await user.save();
+
+//   res.status(200).json(user);
+// };
+
+// //GET USER BY ID
+// const getUserById = async (req, res) => {
+//   const { id } = req.params;
+
+//   try {
+//     const user = await User.findById(id).select("-password");
+
+//     //check if user exists
+//     if (!user) {
+//       res.status(404).json({ errors: ["User not exist"] });
+//       return;
+//     }
+
+//     res.status(200).json(user);
+//   } catch (error) {
+//     res.status(404).json({ errors: ["User not exist"] });
+//   }
+// };
 
 module.exports = {
   add,
--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,7 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
 const jwtSecret = process.env.JWT_SECRET;
 
 //Generate user token
