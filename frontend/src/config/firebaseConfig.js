0955d894635784dcdf6fd193f7817da23853c469
antenorq <antenorq@gmail.com>
2024-01-16 16:00:49 -0500

    updated the file firebaseConfig

--- frontend/src/config/firebaseConfig.js
+++ frontend/src/config/firebaseConfig.js
@@ -2,13 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyBYXrQQwgP5Yw0RA58ityEagWwNzgnI2PI",
-  // authDomain: "quotation-tracker.firebaseapp.com",
-  // projectId: "quotation-tracker",
-  // storageBucket: "quotation-tracker.appspot.com",
-  // messagingSenderId: "535313232033",
-  // appId: "1:535313232033:web:5172eeef156e1409e800e0",
-
   apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
